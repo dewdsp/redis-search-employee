f702fe9fd241773af48feb046f75169502794598
Sitthakarn Prasomsup <sitthakarn.p@gmail.com>
2022-07-13 21:49:53 +0700

    add employee

--- employee/upload_employee.py
+++ employee/upload_employee.py
@@ -1,7 +1,14 @@
 import csv
+import logging
+import os
+from dotenv import load_dotenv
+
+import aioredis
+from aredis_om import Migrator, get_redis_connection
 
 from employee import Employee
-from redis_om import Migrator
+
+config = load_dotenv(".env")
 
 with open("employee.csv") as csv_file:
     employees = csv.DictReader(csv_file)
@@ -11,4 +18,16 @@ with open("employee.csv") as csv_file:
         print(f"{employee['firstname']} has pk = {emp.pk}")
         emp.save()
 
+async def startup():
+    await Migrator().run()
+    logger = logging.getLogger("employee.info")
+    url = os.getenv('REDIS_OM_URL')
+    if not url:
+        url = 'redis://default:mypassword@localhost:6379'
+        logger.info("Using local redis")
+    else:
+        logger.info("Using redis from REDIS_OM_URL")
+
+    r = aioredis.from_url(url, encoding="utf8", decode_responses=True)
+    # FastAPICache.init(RedisBackend(r), prefix="fastapi-cache")
 Migrator().run()
